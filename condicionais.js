c75950d82657bc0fb39aafdb8f5db812e3832aea
Diretorio_TH0 <83072024+thi2556@users.noreply.github.com>
2024-12-07 20:28:44 +0000

    att

--- condicionais.js
+++ condicionais.js
@@ -1,8 +1,20 @@
-var idade = 22
+var idade = 65
+console.log(`Você tem ${idade}!!`)
 if (idade < 16) {
     console.log('Não vota')
-} else if (idade >= 16 && idade < 18) {
+} else if (idade < 18 || idade >= 65) {
     console.log('Voto Opcional')
 } else {
     console.log('Voto Obrigatório!')
+}
+
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12) {
+    console.log('Bom dia!!')
+} else if (hora < 18) {
+    console.log('Boa Tarde!!')
+} else {
+    console.log('Boa Noite')
 }
