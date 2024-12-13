efd6260b35aac0f76f3d8c829a1e8be0bb9c2f41
Diretorio_TH0 <83072024+thi2556@users.noreply.github.com>
2024-12-13 17:30:50 +0000

    att

--- repetição/script.js
+++ repetição/script.js
@@ -33,7 +33,28 @@ function contar() {
 function tabuada() {
     let res = document.getElementById('resTabuada')
     let num1 = document.getElementById('numCalc')
-
-    res.innerHTML = `<h4>O número é: ${num1.value} </h4>`
-
-}
+    res.innerHTML = `<thead class="thead-dark">
+                            <tr>
+                                <th scope="col">Multiplicação</th>
+                                <th scope="col">Resultado</th>
+                            </tr>
+                        </thead>`
+    if (num1.value.length == 0) {
+        res.innerHTML = `<strong>INSIRA UM NÚMERO!!</strong>`
+    } else {
+        num1 = Number(num1.value)
+        for (var t = 1; t <= 10; t++) {
+            // res.innerHTML += `<span class="badge badge-info">${num1} x ${t} = ${num1 * t}</span>`
+            res.innerHTML += `<tbody>
+                            <tr>
+                                <th scope="col">${num1} x ${t}</th>
+                                <th scope="col">${num1 * t}</th>
+                            </tr>
+                        </tbody>`
+        }
+        res.innerHTML += `<tr>
+            <th scope="col">Fim</th>
+            <th scope="col">\u{1F3C1}</th>
+            </tr>`
+    }
+} 
