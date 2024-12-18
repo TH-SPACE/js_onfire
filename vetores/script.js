45582e5b310fc6c6490b87744900b56ca6ae1b98
Diretorio_TH0 <83072024+thi2556@users.noreply.github.com>
2024-12-18 00:44:00 +0000

    att

---
+++ vetores/script.js
@@ -0,0 +1,64 @@
+let num = document.getElementById('num')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let iten = document.createElement('option')
+        iten.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(iten)
+
+    } else {
+        window.alert('Valor invalido ou ja encontrado na lista!!!')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        res.innerHTML = 'Nao contem valores!'
+
+    } else {
+        let total = valores.length
+
+        let maior = valores[0]
+        let menor = valores[0]
+
+        let soma = 0
+
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / total
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${total} numeros cadastrados.</p>`
+        res.innerHTML += `<p>Maior valor informado: ${maior}.</p>`
+        res.innerHTML += `<p>Menor valor informado: ${maior}.</p>`
+        res.innerHTML += `<p>Soma dos valores: ${soma}.</p>`
+        res.innerHTML += `<p>A media: ${Number(media)}.</p>`
+    }
+}
